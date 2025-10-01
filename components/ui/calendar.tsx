7a577f0e1013f23549a1b9bbad3dd7b57b072d04
rytkhs <jag29beem@gmail.com>
2025-10-01 17:00:33 +0900

    fix

--- components/ui/calendar.tsx
+++ components/ui/calendar.tsx
@@ -1,11 +1,12 @@
 "use client";
 
 import * as React from "react";
+
 import { ChevronDownIcon, ChevronLeftIcon, ChevronRightIcon } from "lucide-react";
 import { DayButton, DayPicker, getDefaultClassNames } from "react-day-picker";
 
-import { cn } from "@core/utils";
-import { Button, buttonVariants } from "@/components/ui/button";
+import { cn } from "./_lib/cn";
+import { Button, buttonVariants } from "./button";
 
 function Calendar({
   className,
