3d751d6a250e2c9fe7b1580396b582e332ac0d8d
rytkhs <jag29beem@gmail.com>
2025-10-12 19:22:33 +0900

    fix: imagesを公開

--- middleware.ts
+++ middleware.ts
@@ -93,6 +93,6 @@ export async function middleware(request: NextRequest) {
 export const config = {
   matcher: [
     // APIや静的アセット等は対象外
-    "/((?!api|_next/static|_next/image|favicon.ico|robots.txt|sitemap.xml|manifest.webmanifest).*)",
+    "/((?!api|_next/static|_next/image|favicon.ico|robots.txt|sitemap.xml|manifest.webmanifest|images).*)",
   ],
 };
