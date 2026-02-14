507aa412bad2aa31a6c9d111f027b42f039bbdb5
rytkhs <jag29beem@gmail.com>
2026-02-14 19:54:45 +0900

    chore

--- local_schema.sql
+++ local_schema.sql
@@ -3104,7 +3104,11 @@ ALTER TABLE ONLY "public"."attendances"
 
 
 ALTER TABLE ONLY "public"."events"
-    ADD CONSTRAINT "events_canceled_by_fkey" FOREIGN KEY ("canceled_by") REFERENCES "public"."users"("id");
+    ADD CONSTRAINT "events_canceled_by_fkey" FOREIGN KEY ("canceled_by") REFERENCES "public"."users"("id") ON DELETE SET NULL;
+
+
+
+COMMENT ON CONSTRAINT "events_canceled_by_fkey" ON "public"."events" IS 'Sets canceled_by to NULL if the referenced user is deleted, preventing deletion errors.';
 
 
 
